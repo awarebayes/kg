f42916ca234825ed409c882db665df41e73ad1e5
Mikhails S <awarebayes@gmail.com>
2022-02-12 22:24:00 +0300

    going to recommit

--- 2/main.py
+++ 2/main.py
@@ -1,33 +1,82 @@
+
+from PyQt5.QtWidgets import (QWidget, QPushButton, QLineEdit,
+                             QInputDialog, QApplication, QVBoxLayout, QHBoxLayout, QSlider, QGridLayout)
 import sys
+
+
+#!/usr/bin/python
+
+"""
+ZetCode PyQt5 tutorial
+
+In this example, we draw text in Russian Cylliric.
+
+Author: Jan Bodnar
+Website: zetcode.com
+"""
+
+import sys
+from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtGui import QPainter, QColor, QFont
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QWidget, QLCDNumber, QSlider, QVBoxLayout, QApplication
 
-class Example(QWidget):
 
-    def __init__(self):
-        super(Example, self).__init__()
-        self.initUI()
+class Canvas(QWidget):
 
-    def initUI(self):
+    def __init__(self, parent=None, width=800, height=800):
+        QWidget.__init__(self, parent=parent)
+        vbox = QVBoxLayout(self)
+        self.text = "Canvas is here"
+        self.setMinimumSize(200, 200)
+        self.setMaximumSize(1000, 1000)
 
-        lcd = QLCDNumber(self)
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+        self.drawText(event, qp)
+        qp.end()
+
+    def drawText(self, event, qp):
+        qp.setPen(QColor(168, 34, 3))
+        qp.setFont(QFont('Decorative', 10))
+        qp.drawText(event.rect(), Qt.AlignCenter, self.text)
+
+
+class SidePanel(QWidget):
+    def __init__(self, parent=None):
+        QWidget.__init__(self, parent=parent)
+        layout = QGridLayout(self)
+
+        okButton = QPushButton("OK")
+        cancelButton = QPushButton("Cancel")
+
         sld = QSlider(Qt.Horizontal, self)
+        sld.setFocusPolicy(Qt.NoFocus)
 
-        vbox = QVBoxLayout()
-        vbox.addWidget(lcd)
-        vbox.addWidget(sld)
+        layout.addWidget(okButton, 0, 0)
+        layout.addWidget(cancelButton, 0, 1)
+        layout.addWidget(sld)
 
-        self.setLayout(vbox)
-        sld.valueChanged.connect(lcd.display)
+        self.setMaximumWidth(400)
 
-        self.setGeometry(300, 300, 250, 150)
-        self.setWindowTitle('Signal and slot')
-        self.show()
+
+class View(QWidget):
+
+    def __init__(self, parent=None):
+        QWidget.__init__(self, parent=parent)
+
+        vbox = QHBoxLayout(self)
+        side_panel = SidePanel(self)
+        canvas = Canvas(self)
+        vbox.addWidget(side_panel)
+        vbox.addWidget(canvas)
 
 
 def main():
+
     app = QApplication(sys.argv)
-    ex = Example()
+    ex = View()
+    ex.show()
     sys.exit(app.exec_())
 
 
